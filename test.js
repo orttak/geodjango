9b7d26434a724060423faddef4e021c8665330ed
orttak <orttak@gmail.com>
2021-04-19 09:40:26 +0300

    leaflet fundemental

---
+++ test.js
@@ -0,0 +1,93 @@
+var data ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {name:'test'},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            40.40771484375,
+            37.67077737288316
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            39.781494140625,
+            37.83148014503288
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            40.7098388671875,
+            38.1777509666256
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            39.9627685546875,
+            37.309014074275915
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            40.8966064453125,
+            37.45741810262938
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            40.60272216796875,
+            37.201893907733826
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            41.35528564453125,
+            36.9806150652861
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            41.1712646484375,
+            36.93013456125329
+          ]
+        }
+      }
+    ]
+  }
